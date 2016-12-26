4d24dee628347356cd9289d93fa2c01f6612f582
DevpNarayana <hai.narayn@gmail.com>
2016-12-26 08:37:04 +0530

    Initial commit

---
+++ closures_2.js
@@ -0,0 +1,8 @@
+function multiplier(factor) {
+    return function(number) {
+        return factor * number
+    }
+}
+var twice = multiplier(5);
+console.log(twice(2));
+// console.log(multiplier(5));
